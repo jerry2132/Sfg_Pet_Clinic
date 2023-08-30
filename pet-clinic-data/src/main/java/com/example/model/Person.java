e37ccc7c8256b447266c736dafdbc3797da2e330
jerry2132 <sumitsinghrawat34@gmail.com>
2023-08-30 17:41:02 +0530

    commit applied

--- pet-clinic-data/src/main/java/com/example/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/model/Person.java
@@ -1,6 +1,6 @@
 package com.example.model;
 
-public class Person {
+public class Person extends BaseEntity{
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
