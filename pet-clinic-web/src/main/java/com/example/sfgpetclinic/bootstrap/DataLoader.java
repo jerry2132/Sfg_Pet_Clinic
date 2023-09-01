f51e3bb702d31a59e87da5d51f7d51651930eb0c
jerry2132 <sumitsinghrawat34@gmail.com>
2023-09-01 16:22:32 +0530

    commit applied

--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/bootstrap/DataLoader.java
@@ -29,9 +29,9 @@ public class DataLoader implements CommandLineRunner {
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner1.setId(2L);
-        owner1.setFirstName("Fionna");
-        owner1.setLastName("Gelannen");
+        owner2.setId(2L);
+        owner2.setFirstName("Fionna");
+        owner2.setLastName("Gelannen");
 
         ownerService.save(owner2);
         System.out.println("Loaded Owners...");
--- pet-clinic-web/src/main/java/com/example/sfgpetclinic/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/com/example/sfgpetclinic/controller/OwnerController.java
@@ -1,14 +1,23 @@
 package com.example.sfgpetclinic.controller;
 
+import com.example.sfgpetclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping({"/owner"})
 @Controller
 public class OwnerController {
 
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
     @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listOwner()
+    public String listOwner(Model model)
     {
+        model.addAttribute("owner", ownerService.findAll());
         return "owner/index";
     }
 }
