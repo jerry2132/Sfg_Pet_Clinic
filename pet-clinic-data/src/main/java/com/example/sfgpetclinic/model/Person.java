0f5a42f59d279a9a64167784acce1827616663c8
jerry2132 <sumitsinghrawat34@gmail.com>
2023-09-12 16:00:38 +0530

    commit applied

--- pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Person.java
+++ pet-clinic-data/src/main/java/com/example/sfgpetclinic/model/Person.java
@@ -1,10 +1,17 @@
 package com.example.sfgpetclinic.model;
 
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
 public class Person extends BaseEntity{
+    @Column(name = "first_name")
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    @Column(name = "last_name")
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
